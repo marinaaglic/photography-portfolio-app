375d20fa6a9a67e001832197876d4378fb8fe8be
marinaaglic <marinaaglic11@gmail.com>
2024-10-21 16:49:55 +0200

    styling added

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,5 +1,36 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav>
+        <ul style={ulStyle}>
+          <li style={liStyle}>Photography Portfolio</li>
+          <li style={liStyle}>Sunset Pictures</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
+const headerStyle: React.CSSProperties = {
+  padding: "10px 20px",
+  backgroundColor: "#222",
+  color: "#fff",
+  textAlign: "center",
+};
+
+const ulStyle: React.CSSProperties = {
+  listStyle: "none",
+  display: "flex",
+  justifyContent: "space-around",
+  padding: "0",
+};
+
+const liStyle: React.CSSProperties = {
+  margin: "0 15px",
+};
+
+const linkStyle: React.CSSProperties = {
+  color: "#fff",
+  textDecoration: "none",
+};

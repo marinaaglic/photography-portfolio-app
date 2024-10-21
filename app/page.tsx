87ceed94c35b8d54d9cd0a660211045964d9a692
marinaaglic <marinaaglic11@gmail.com>
2024-10-21 16:25:08 +0200

    added header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Header from "../components/header/Header";
+
 export default function Home() {
-  return <div>Photography Portfolio</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }

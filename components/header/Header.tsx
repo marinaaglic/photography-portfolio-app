70ea1f23c75b41892998f253d9867400e25b8aaf
marinaaglic <marinaaglic11@gmail.com>
2024-10-22 18:35:46 +0200

    updated styling of header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,36 +1,15 @@
 import React from "react";
+import styles from "./header.module.css";
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <nav>
-        <ul style={ulStyle}>
-          <li style={liStyle}>Photography Portfolio</li>
-          <li style={liStyle}>Sunset Pictures</li>
+        <ul className={styles.ul}>
+          <li className={styles.li}>Photography Portfolio</li>
+          <li className={styles.li}>Sunset Pictures</li>
         </ul>
       </nav>
     </header>
   );
 }
-const headerStyle: React.CSSProperties = {
-  padding: "10px 20px",
-  backgroundColor: "#222",
-  color: "#fff",
-  textAlign: "center",
-};
-
-const ulStyle: React.CSSProperties = {
-  listStyle: "none",
-  display: "flex",
-  justifyContent: "space-around",
-  padding: "0",
-};
-
-const liStyle: React.CSSProperties = {
-  margin: "0 15px",
-};
-
-const linkStyle: React.CSSProperties = {
-  color: "#fff",
-  textDecoration: "none",
-};

d0bf4c64490acc033e1a8385d6e70a0fde10aa8f
marinaaglic <marinaaglic11@gmail.com>
2024-11-20 17:00:51 +0100

    added about page and json with data for the page

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./page.module.css";
+import Header from "../../components/header/Header";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <Header />
+      <div>
+        <p></p>
+      </div>
+    </div>
+  );
+}
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import styles from "./header.module.css";
 import { useTheme } from "../../app/providers/ThemeProvider";
 import ThemeToggle from "../toggle/ThemeToggle";
+import Link from "next/link";
 
 export default function Header() {
   const { theme, toggleTheme } = useTheme();
@@ -14,6 +15,7 @@ export default function Header() {
       <nav>
         <ul className={styles.ul}>
           <li className={styles.li}>Photography Portfolio</li>
+          <Link href="/about">About</Link>
           <li className={styles.li}>
             <ThemeToggle onToggle={toggleTheme} theme={theme} />
           </li>

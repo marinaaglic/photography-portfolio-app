7a0b9639d0e6b0d038b4c8409b9b4d12050e2a69
marinaaglic <marinaaglic11@gmail.com>
2024-11-30 18:03:34 +0100

    moved previous and next button to another component

---
+++ components/reusable/NavButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import styles from "./NavButton.module.css";
+
+interface NavButtonProps {
+  direction: "left" | "right";
+  onClick: () => void;
+}
+
+export default function NavButton({ direction, onClick }: NavButtonProps) {
+  const Icon = direction === "left" ? GrFormPrevious : GrFormNext;
+  return (
+    <Icon
+      className={`${styles.navButton} ${
+        direction === "left" ? styles.left : styles.right
+      }`}
+      onClick={onClick}
+    />
+  );
+}
